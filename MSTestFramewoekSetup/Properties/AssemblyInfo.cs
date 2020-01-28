fd26c5f6be650273130919388fe17ce11e1bb65d
Oladeni1 <olatundeoladeni@yahoo.com>
2020-01-28 13:35:53 +0000

    Final commit MSTest Framework

---
+++ MSTestFramewoekSetup/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MSTestFramewoekSetup")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MSTestFramewoekSetup")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f113dbd8-d614-47ad-b1ad-5c4a836da470")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MSTestFramewoekSetup/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MSTestFramewoekSetup
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [ClassInitialize]
+        public static void BeforeClass(TestContext tc)
+        {
+            Console.WriteLine("This runs before Class");
+        }
+        [ClassCleanup]
+        public static void AfterClass()
+        {
+            Console.WriteLine("This runs after Class");
+        }
+        [TestInitialize]
+        public void BeforeTest()
+        {
+            Console.WriteLine("This runs before Test");
+        }
+        [TestCleanup]
+        public void AfterTest()
+        {
+            Console.WriteLine("This runs after Test");
+        }
+        [TestMethod, TestCategory("Smoke Test")]
+        public void TestMethod1()
+        {
+            Console.WriteLine("This runs the Test, Test category");
+        }
+    }
+}
